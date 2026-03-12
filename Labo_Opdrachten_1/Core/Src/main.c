eed0b5776762175f136a3efaab7ef33c4b7bfa23
Gdoc141 <gealdoc141@gmail.com>
2026-03-13 00:00:17 +0100

    Update main.c

--- Labo_Opdrachten_1/Core/Src/main.c
+++ Labo_Opdrachten_1/Core/Src/main.c
@@ -37,6 +37,38 @@
 #include "stm32h5xx_nucleo.h"   // BSP LED helpers (je project compileert stm32h5xx_nucleo.c al)
 // Note: SPI HAL driver is not included in this project, so we use bit-bang SPI via GPIO
 
+/*
+ * BELANGRIJKSTE LIJNEN OM TE KENNEN
+ *
+ * Overzicht hoofdflow:
+ * - Lijn 203-235: main() initialiseert GPIO, bit-bang SPI, USB en MCP23S17.
+ * - Lijn 228-234: hoofdloop met tud_task(), keypad_task() en midi_task().
+ *
+ * MCP23S17 <-> STM32 communicatie:
+ * - Lijn 505-527: MX_SPI_BitBang_Init() zet PA5/PA6/PA7 als SCK/MISO/MOSI.
+ * - Lijn 533-558: spi_transfer_byte() stuurt 1 byte via MOSI en leest tegelijk 1 byte via MISO.
+ * - Lijn 671-683: mcp23s17_write_reg() schrijft een register naar de MCP23S17 via SPI.
+ * - Lijn 690-704: mcp23s17_read_reg() leest een register van de MCP23S17 via SPI.
+ * - Lijn 710-719: mcp23s17_init() configureert GPIOA als input en GPIOB als output.
+ *
+ * Waar leest de code de binnengekomen SPI-waarde om te weten welke knop ingedrukt is:
+ * - Lijn 748: keypad_scan() activeert telkens 1 rij van de matrix.
+ * - Lijn 757-758: read1/read2 = mcp23s17_read_reg(MCP_REG_GPIOA) leest de kolommen in.
+ * - Lijn 769: keypad_state[row] = read1 bewaart de stabiele kolomwaarde voor die rij.
+ * - Betekenis van die waarde: bit = 0 betekent toets ingedrukt, bit = 1 betekent toets los.
+ *   Omdat maar 1 rij tegelijk actief laag wordt gemaakt, weet je zo exact welke toets [row][col] actief is.
+ *
+ * Vertaling van matrixwaarde naar knop en MIDI-noot:
+ * - Lijn 125-129: note_map[row][col] vertaalt rij + kolom naar een MIDI-noot.
+ * - Lijn 340: keypad_task() start de interpretatie van keypad_state.
+ * - Lijn 367-368: current_bit en prev_bit halen per kolom de bitwaarde uit keypad_state/keypad_prev.
+ * - Lijn 372: prev_bit && !current_bit = toets werd net ingedrukt.
+ * - Lijn 374: note = note_map[row][col] vertaalt de gedetecteerde knop naar een MIDI-noot.
+ * - Lijn 385: tud_midi_packet_write(note_on) verstuurt de Note On naar de pc.
+ * - Lijn 391: !prev_bit && current_bit = toets werd losgelaten.
+ * - Lijn 403: tud_midi_packet_write(note_off) verstuurt de Note Off.
+ */
+
 //--------------------------------------------------------------------+
 // HAL / CubeMX prototypes
 //--------------------------------------------------------------------+
